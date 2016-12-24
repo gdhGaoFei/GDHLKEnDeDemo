f7975d966c09ecd73df07eeda1189396bea667cb
高得华 <964195787@qq.com>
2016-12-24 23:00:36 +0800

    上传代码 ===== 完成基本工程

---
+++ GDHLKEnDeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GDHLKEnDeDemo
+//
+//  Created by 高得华 on 16/12/24.
+//  Copyright © 2016年 GaoFei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GDHLKEnDeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GDHLKEnDeDemo
+//
+//  Created by 高得华 on 16/12/24.
+//  Copyright © 2016年 GaoFei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
